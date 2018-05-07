d246b8edc59889d7d63b28e2d6d1547332e6b1b4
Fathur <Fathur>
2018-05-07 12:01:59 +0700

    First commit

---
+++ database/migrations/2018_05_02_041637_create_data_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('db_logger', function (Blueprint $table) {
+            $table->increments('id');
+            $table->time('time');
+            $table->integer('queue_length');
+            $table->integer('green_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('database');
+    }
+}
